90ed1ff8625d4199cb3ee962bfa4a3670f6f4006
acantrell2009 <67588855+acantrell2009@users.noreply.github.com>
2020-08-20 01:11:56 +0000

    Update install-labtainer.sh

--- install-labtainer.sh
+++ install-labtainer.sh
@@ -35,17 +35,16 @@ read -p "This script will reboot the system when done, press enter to continue"
 #
 target=~/.bashrc
 grep ":./bin:" $target | grep PATH >>/dev/null
+result=$?
 if [[ result -ne 0 ]];then
    here=`pwd`
    cat <<EOT >>$target
    if [[ ":\$PATH:" != *":./bin:"* ]]; then 
        export PATH="\${PATH}:./bin:$here/trunk/scripts/designer/bin"
-       export LABTAINER_DIR=$here/trunk
+       export LABTAINER_DIR=$PWD/trunk
    fi
 EOT
 fi
-
-echo "LABTAINER_DIR=$PWD/trunk" >> ~/.bashrc
 
 if [ ! -h labtainer-student ]; then ln -s trunk/scripts/labtainer-student; fi
 if [ ! -h labtainer-instructor ]; then ln -s trunk/scripts/labtainer-instructor; fi
@@ -63,7 +62,7 @@ if [[ -z "$1" ]]; then
 else
     distrib=$1
 fi
-
+RESULT=0
 case "$distrib" in
     Ubuntu)
         echo is ubuntu
@@ -93,7 +92,6 @@ case "$distrib" in
         fi
         exit 1
 esac
-RESULT=0
 if [[ "$RESULT" -eq 0 ]]; then
     /usr/bin/newgrp docker <<EONG
     /usr/bin/newgrp $USER 
